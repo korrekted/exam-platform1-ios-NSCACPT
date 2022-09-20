08835a51a70288a0a9ccef05aaed9b59e5a02c39
akonst17 <akonst17@gmail.com>
2022-09-20 18:35:06 +0300

    интеграция sdk 2.0; новые урлы; build 8

--- NSCACPT/Classes/Misc/GlobalDefinitions.swift
+++ NSCACPT/Classes/Misc/GlobalDefinitions.swift
@@ -6,21 +6,24 @@
 //
 
 final class GlobalDefinitions {
-    static let domainUrl = "https://exams.korrekted.com"
+    static let domainUrl = "https://exams3.mastrapi.com"
     static let apiKey = "AbAxBsSXdfJxXNa6"
-
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "umkElTr0KwqXps81"
 
     static let applicationTag = "EXAM_NSCA_CPT"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "umkElTr0KwqXps81"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "3BJL2NTRsj9BI0ZS"
 
     static let appStoreUrl = "https://apps.apple.com/app/nsca-cpt-fitness-prep/id1580267551"
     static let appleAppID = "1580267551"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
